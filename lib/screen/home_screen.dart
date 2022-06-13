4851bb01717abce3ae012491b9646daa42aeb14b
kimyejin <7602kyj@e-mirim.hs.kr>
2022-06-13 17:45:52 +0900

    폴더 기본구조 설계

---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class HomeScreen extends StatefulWidget {
+
+  @override
+  State<HomeScreen> createState() => _HomeState();
+}
+
+class _HomeState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
